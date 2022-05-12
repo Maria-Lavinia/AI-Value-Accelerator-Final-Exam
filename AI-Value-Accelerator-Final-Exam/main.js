728e56d6d6bdd04d3b6f9d8eb622944e24406fd5
Helena <hele362c@stud.kea.dk>
2022-05-12 17:48:05 +0200

    login page

--- AI-Value-Accelerator-Final-Exam/main.js
+++ AI-Value-Accelerator-Final-Exam/main.js
@@ -1,3 +1,14 @@
-
-
+"use strict";
 //login
+
+// function validateForm() {
+//     var x = document.forms["myForm"]["username"]["password"].value;
+//     if (x == "") {
+//       return false;
+//     }
+//   }
+//  const form = document.querySelector("#myForm");
+//  form.addEventListener("submit", (e) =>{
+//    e.preventDefault();
+//    onchange="try{setCustomValidity('')}catch(e)"
+//  })
