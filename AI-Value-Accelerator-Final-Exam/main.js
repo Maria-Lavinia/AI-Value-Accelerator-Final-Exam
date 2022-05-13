4844238ca5b1af2540dc330aa186f9410648cfa0
Maria-Lavinia Otelea <mlo@frankly.dk>
2022-05-13 11:32:34 +0200

    switch main

--- AI-Value-Accelerator-Final-Exam/main.js
+++ AI-Value-Accelerator-Final-Exam/main.js
@@ -1,4 +1,18 @@
 //login
+"use strict";
+//login
+
+// function validateForm() {
+//     var x = document.forms["myForm"]["username"]["password"].value;
+//     if (x == "") {
+//       return false;
+//     }
+//   }
+//  const form = document.querySelector("#myForm");
+//  form.addEventListener("submit", (e) =>{
+//    e.preventDefault();
+//    onchange="try{setCustomValidity('')}catch(e)"
+//  })
 
 //game
 
@@ -30,17 +44,3 @@ function init() {
 
   console.log(init());
   //end of loading anim
-"use strict";
-//login
-
-// function validateForm() {
-//     var x = document.forms["myForm"]["username"]["password"].value;
-//     if (x == "") {
-//       return false;
-//     }
-//   }
-//  const form = document.querySelector("#myForm");
-//  form.addEventListener("submit", (e) =>{
-//    e.preventDefault();
-//    onchange="try{setCustomValidity('')}catch(e)"
-//  })
