04a485d8f11ea3144c63dd035b97efc920330e68
Maria-Lavinia Otelea <otelea.lavi@gmail.com>
2022-05-13 10:56:38 +0200

    changes

--- AI-Value-Accelerator-Final-Exam/main.js
+++ AI-Value-Accelerator-Final-Exam/main.js
@@ -1,3 +1,32 @@
-
-
 //login
+
+//game
+
+function init() {
+  
+    const screen = document.querySelector(".game");
+    const loader = document.querySelector(".number");
+    screen.style.display = "none";
+    
+    const properties = {
+    duration: 1000,
+    iteration: 1,
+    
+    }
+
+    const keyframes = [
+         {transform: 'rotateY(90deg)'},
+        {transform: 'rotateY(-90deg)'}
+    ]
+
+    const animation = loader.animate(keyframes, properties)
+
+    setTimeout(() => {
+      animation.style.display = "none";
+      animation.style.visibility = "hidden";
+    }, 3000);
+  
+  }
+
+  console.log(init());
+  //end of loading anim
