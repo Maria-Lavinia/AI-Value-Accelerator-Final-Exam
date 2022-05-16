692e6d7e55ee2003c295b7c4b01f2222bfa56813
Maria-Lavinia Otelea <mlo@frankly.dk>
2022-05-16 12:47:10 +0200

    Making the loading animation and adding the icons

--- AI-Value-Accelerator-Final-Exam/main.js
+++ AI-Value-Accelerator-Final-Exam/main.js
@@ -1,2 +1,35 @@
 
 "use strict";
+window.addEventListener("DOMContentLoaded", loading_anim());
+
+function loading_anim(){
+    const number = document.querySelector(".number");
+    const game = document.querySelector(".game_board");
+    const dice = document.querySelector("#ui_dado");
+    const roll = document.querySelector(".rollme");
+    const loader = document.querySelector(".loading-animation");
+    const blur = document.querySelector(".blur");
+    blur.style.visibility="visible";
+    number.style.display="visible";
+    game.style.display="none";
+    game.style.visibility="hidden";
+    dice.style.display="none"
+    dice.style.visibility="hidden";
+    roll.style.display="none";
+    roll.style.visibility="hidden";
+    setTimeout(() => {
+        blur.style.visibility="hidden";
+        blur.style.display="none"
+        number.style.display="none";
+        loader.style.display="none"
+        number.style.visibility="hidden"
+        game.style.display="block";
+        game.style.visibility="visible";
+        dice.style.display="block";
+        dice.style.visibility="visible";
+        roll.style.display="block";
+        roll.style.visibility="visible";
+    }, 3500);
+}
+
+console.log(loading_anim());
