1e1fbcb140deb296d7a96a10cdd51f57167491e9
Helena <hele362c@stud.kea.dk>
2022-05-24 14:10:41 +0200

    working on insigts

--- AI-Value-Accelerator-Final-Exam/script.js
+++ AI-Value-Accelerator-Final-Exam/script.js
@@ -1,49 +1,50 @@
 "use strict";
 
-
-const form = document.querySelector(".formz");
-document.querySelector(".insButton").addEventListener("click", showForm);
-
-
-function showForm(){
-    document.querySelector(".endddScreenz").classList.add("hidden");
-    document.querySelector(".formz").classList.remove("hidden")
-}
+document.querySelector(".endScreenBtn")
 
 // const form = document.querySelector(".formz");
+// document.querySelector(".insButton").addEventListener("click", showForm);
 
-form.addEventListener("submit", handleSubmit);
 
-function handleSubmit(e){
-    e.preventDefault();
-    document.querySelector(".formz").classList.add("hidden")
-   const payload = {
-    content: form.elements.content.value,
-   };
+// function showForm(){
+//     document.querySelector(".endddScreenz").classList.add("hidden");
+//     document.querySelector(".formz").classList.remove("hidden")
+// }
 
-   document.querySelector("input[type=submit]").disabled = true;
+// // const form = document.querySelector(".formz");
+
+// form.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(e){
+//     e.preventDefault();
+//     document.querySelector(".formz").classList.add("hidden")
+//    const payload = {
+//     content: form.elements.content.value,
+//    };
+
+//    document.querySelector("input[type=submit]").disabled = true;
   
-   console.log(payload);
-   fetch(`https://portfolio-8a02.restdb.io/rest/final-exam-2`, {
-      method: "POST",
-      headers: {
-        "x-apikey": "61753a458597142da1745988",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    })
-    .then(res=>res.json())
-    .then((data) => {
+//    console.log(payload);
+//    fetch(`https://portfolio-8a02.restdb.io/rest/final-exam-2`, {
+//       method: "POST",
+//       headers: {
+//         "x-apikey": "61753a458597142da1745988",
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(payload),
+//     })
+//     .then(res=>res.json())
+//     .then((data) => {
   
-      const template = document.querySelector("template.insightsTemplate").content;
-      const copy = template.cloneNode(true);
-      copy.querySelector(".line").textContent = data.content;
-      document.querySelector(".mainContainer").appendChild(copy);
+//       const template = document.querySelector("template.insightsTemplate").content;
+//       const copy = template.cloneNode(true);
+//       copy.querySelector(".line").textContent = data.content;
+//       document.querySelector(".mainContainer").appendChild(copy);
     
      
-      document.querySelector("input[type=submit]").disabled = false;
+//       document.querySelector("input[type=submit]").disabled = false;
   
-      form.elements.content.value = "";
-    });
-  } 
+//       form.elements.content.value = "";
+//     });
+//   } 
   
