b80f2e54201b77f447273328e652dcc252f66b46
Maria-Lavinia Otelea <mlo@frankly.dk>
2022-05-31 00:28:46 +0200

    attempt 3

--- AI-Value-Accelerator-Final-Exam/main.js
+++ AI-Value-Accelerator-Final-Exam/main.js
@@ -85,11 +85,12 @@ function showModal(){
     isModalOpen = true;
     restartTimer();
     this.querySelector(".popup").classList.remove("hide");
-    const gameFigure = document.createElement("img");
-    gameFigure.src = "public/assets/gameFigure.png"
-    gameFigure.style.width = "2rem";
+    // const gameFigure = document.createElement("img");
+    // gameFigure.src = "public/assets/gameFigure.png"
+    // gameFigure.style.width = "2rem";
     let clickedSquare = this.querySelector('.popup').parentNode;
     popupSound.play();
+    changeColor();
     
     const refreshIntervalId = setInterval(updateCountdown, 1000);
     
@@ -129,14 +130,20 @@ function unmuteSound() {
 }
 
 
+// const squareArray = document.querySelectorAll(".square");
+// const gameFigure = document.createElement("img");
+// gameFigure.src = "public/assets/gameFigure.png"
+// gameFigure.style.width = "2rem"
+
+
 const squareArray = document.querySelectorAll(".square");
 const gameFigure = document.createElement("img");
 gameFigure.src = "public/assets/gameFigure.png"
 gameFigure.style.width = "2rem"
 
-
-
 function changeColor(){
+
+
 squareArray.forEach((square) => {
     square.addEventListener('focus', (event) => {
         //event.target.style.backgroundColor = 'pink';
@@ -144,13 +151,13 @@ squareArray.forEach((square) => {
       }, true);
     })
   }
+  console.log(changeColor());
 
   // displaying the answers to the questions
 
   document.querySelector(".finfish-button").addEventListener("click", e =>{
     e.preventDefault();
     const textareas = document.querySelectorAll("textarea");
-    const header = document.querySelectorAll("header");
     console.log(textareas);
 
     const newArray = [...textareas].map(area => area.value)
@@ -160,77 +167,3 @@ squareArray.forEach((square) => {
   })
 
 
-// const square2 = document.querySelectorAll(".square");
-// square2.forEach((apple) => apple.addEventListener("click", showColor));
-//   }
-    
-
-// function showColor(){
-//    //this.style.backgroundColor = "rgba(180, 180, 180)";
-//    this.addEventListener('focus', (event) => {
-//             //event.target.style.backgroundColor = 'pink';
-//           }, true);
-//           this.addEventListener('blur', (event) => {
-//                     event.target.style.backgroundColor = '#04304A';
-//                     event.target.style.border = '6px solid black';
-                    
-//                   }, true);
-//   }
-
-
-
-
-//     ;})
-
-//square2.forEach((apple) => apple.addEventListener("click", showColor));
-
-
-/* square2.addEventListener('focus', (event) => {
-    event.target.style.backgroundColor = 'pink';
-  }, true); */
-
-/* square2.addEventListener('blur', (event) => {
-    event.target.style.backgroundColor = 'rgba(180, 180, 180)';
-    
-  }, true); */
-
-
-/* function showColor(){
-
-    document.querySelector(".square").addEventListener('focus', (event) => {
-        event.target.style.backgroundColor = 'pink';
-      });
-      
-   this.style.backgroundColor = "rgba(180, 180, 180)";
-
-   this.style.border = "rgba(174, 174, 240)";
-   showForm();
-   this.style.opacity = "50%"; 
-   document.querySelector(".popup").style.pointerEvents = "none";
-  } */
-
-
-
-//   function showTime() {
-//     console.log("showTime");
-//     if (timeLeft > 0) {
-//         timeLeft--;
-//         startTimer();
-//         document.querySelector("#time").textContent = timeLeft;
-//     } else {
-//         youLost();
-//     }
-// }
-
-// function startTimer() {
-//     console.log("startTimer");
-
-   
-
-//     if (timeLeft == 0) {
-//         youLost();
-//     } else {
-//         setTimeout (showTime, 1000);
-//     }
-    
-// }
